c3a3a4a5bc3d691d4d3a5cd32b99613cd0ce523b
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2021-05-05 22:02:18 -0300

    Add endpoints to list, edit and delte materials

--- composter/urls.py
+++ composter/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.conf.urls import url
 from .views import *
 from rest_framework.routers import DefaultRouter
 
@@ -6,6 +7,8 @@ router = DefaultRouter()
 router.register(r'material_types', MaterialTypeViewSet, basename='material_types')
 
 urlpatterns = [
-    path('register_material/', registerMaterial, name='register_material'),
+    path('register_material/', registerMaterial, name='register-material'),
+    path('materials/', materials, name='materials-list'),
     path('', include(router.urls)),
+    url(r'update_material/(?P<id>[A-Za-z0-9]+)$', updateMaterial, name='update-material'),
 ]
--- composter/views.py
+++ composter/views.py
@@ -6,9 +6,11 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.renderers import JSONRenderer
 from rest_framework.status import (
-    HTTP_500_INTERNAL_SERVER_ERROR,
+    HTTP_200_OK,
+    HTTP_201_CREATED,
     HTTP_400_BAD_REQUEST,
-    HTTP_201_CREATED
+    HTTP_405_METHOD_NOT_ALLOWED,
+    HTTP_500_INTERNAL_SERVER_ERROR,
 )
 from djongo import *
 from .models import *
@@ -28,8 +30,8 @@ def registerMaterial(request):
         material_type = MaterialType.objects.get(_id=material_type_id)
     except Exception:
         return Response(
-            {'error': 'Tipo de material inválido'},
-            status=HTTP_500_INTERNAL_SERVER_ERROR
+            {'error': 'Tipo de material - ID inválido'},
+            status=HTTP_400_BAD_REQUEST
         )
 
     try:
@@ -49,3 +51,77 @@ def registerMaterial(request):
         response_data,
         status=HTTP_201_CREATED
     )
+
+@api_view(["GET"])
+def materials(request):
+    materials = Material.objects.all()
+    response_data = MaterialSerializer(materials, many=True).data
+
+    for each in response_data:
+        each['materialType'] = str(each['materialType'])
+
+    return Response(
+        response_data,
+        status=HTTP_201_CREATED
+    )
+
+@api_view(["PUT", "DELETE"])
+def updateMaterial(request, id):
+    try:
+        material_id = ObjectId(id)
+        material = Material.objects.get(_id=material_id)
+    except Exception:
+        return Response(
+            {'error': 'Material - ID inválido'},
+            status=HTTP_400_BAD_REQUEST
+        )
+
+    if request.method == "DELETE":
+        material.delete()
+        return Response(
+            {'OK':'Deleted'},
+            status=HTTP_200_OK
+        )
+
+    elif request.method == "PUT":
+        try:
+            if request.data['name']:
+                material.name = request.data['name']
+        except Exception:
+            pass 
+
+        try:
+            if request.data['imageLink']:
+                material.imageLink = request.data['imageLink']
+        except Exception:
+            pass 
+
+        try:
+            if request.data['materialType']:
+                try:
+                    material_type_id = ObjectId(request.data['materialType'])
+                    material_type = MaterialType.objects.get(_id=material_type_id)
+                except Exception:
+                    return Response(
+                        {'error': 'Tipo de material - ID inválido'},
+                        status=HTTP_400_BAD_REQUEST
+                    ) 
+
+                material.materialType = material_type
+        except Exception:
+            pass 
+
+        material.save()
+
+        response_data = MaterialSerializer(material).data
+        response_data['materialType'] = str(response_data['materialType'])
+
+        return Response(
+            response_data,
+            status=HTTP_200_OK
+        )
+    else:
+        return Response(
+            {'error': 'Método não permitido'},
+            status=HTTP_405_METHOD_NOT_ALLOWED
+        )
